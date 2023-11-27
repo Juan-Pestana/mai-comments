6fc234efced59ca1187ccc24cc565d5b9f1d6127
Juan Pestana <juan.pestana82@gmail.com>
2023-11-27 10:03:00 +0100

    testing

--- src/components/ResponsiveTextArea.tsx
+++ src/components/ResponsiveTextArea.tsx
@@ -74,7 +74,7 @@ const ResponsiveTextArea = ({ session }: { session: any }) => {
 
   const handleLogin = () => {
     const newWindow = window.open(
-      `http://localhost:3000/auth/signin?callbackUrl=http://127.0.0.1:5500/index.html`,
+      `https://mai-comments.vercel.app//auth/signin?callbackUrl=http://127.0.0.1:5500/index.html`,
       //    `http://localhost:3000/auth/signin?callbackUrl=${window.parent.location.href}`,
       '_blank'
     )
