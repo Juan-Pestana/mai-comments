2ce9487e3454c5a45fbffce7da6b4e2f4b3ca983
Juan Pestana <juan.pestana82@gmail.com>
2024-06-10 00:53:42 +0200

    tratarad

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import { data } from '@/db/data'
 import { auth, signIn, signOut } from '../../auth'
 import { Button } from '@/components/ui/button'
 
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 const asignResponse = (id: number, comments: Icomment[]) => {
   const response: IordComment[] = comments
     .filter((c) => c.parent === id)
